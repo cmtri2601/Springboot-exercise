3bd21bf0d7836ac323a5085ba9c73c2e97112991
tri <cmtri601@gmail.com>
2025-06-16 10:27:50 +0700

    refactor code

--- cron/src/main/java/nc/solon/cron/service/ConsumeTaxCalculation.java
+++ cron/src/main/java/nc/solon/cron/service/ConsumeTaxCalculation.java
@@ -12,24 +12,24 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ConsumeTaxCalculation {
 
-    private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-    @Value("${person-service.consume-url}")
-    String consumeUrl;
+  @Value("${person-service.consume-url}")
+  String consumeUrl;
 
-    public ConsumeTaxCalculation(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
+  public ConsumeTaxCalculation(RestTemplateBuilder builder) {
+    this.restTemplate = builder.build();
+  }
+
+  @Scheduled(fixedRateString = "${fixed-rate.consume-tax-calculation}")
+  public void callRestEndpoint() {
+
+    try {
+      ResponseEntity<String> response = restTemplate.getForEntity(consumeUrl, String.class);
+      log.info("Called endpoint. Status: {}", response.getStatusCode());
+      log.info("Response: {}", response.getBody());
+    } catch (Exception e) {
+      log.info("Failed to call endpoint: {}", e.getMessage());
     }
-
-    @Scheduled(fixedRateString = "${fixed-rate.consume-tax-calculation}")
-    public void callRestEndpoint() {
-
-        try {
-            ResponseEntity<String> response = restTemplate.getForEntity(consumeUrl, String.class);
-            log.info("Called endpoint. Status: {}", response.getStatusCode());
-            log.info("Response: {}", response.getBody());
-        } catch (Exception e) {
-            log.info("Failed to call endpoint: {}", e.getMessage());
-        }
-    }
+  }
 }
