e5c8c76acbfa3558838e942ac99a9fd4e64945db
tri <cmtri601@gmail.com>
2025-06-18 15:55:07 +0700

    init camunda

---
+++ camunda/src/test/java/nc/solon/camunda/CamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.solon.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
